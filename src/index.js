6e398f6fe0133c55f6cd7917495b405ba0d8887f
Ksigei <sigeikiprono4@gmail.com>
2022-05-09 11:39:37 +0300

    Update the index.js

--- src/index.js
+++ src/index.js
@@ -1,89 +1,105 @@
-import './style.css';
+import "./style.css";
 
 const acctBalanceLbl = document.getElementById("acctBalanceLbl");
-const deposits = [];
-const withdrawals = [];
-let totalBalance = 25;
+// const deposits = [];
+// const withdrawals = [];
+// let totalBalance = 25;
 const userDeposit = document.getElementById("userDeposit");
 const btnDeposit = document.getElementById("btnDeposit");
 const userWithdraw = document.getElementById("userWithdraw");
 const btnWithdraw = document.getElementById("btnWithdraw");
 
-// Create our number formatter.
-const formatter = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-    minimumFractionDigits: 2,
-    /* 
-    the default value for minimumFractionDigits depends on the currency
-    and is usually already 2
-    */
+function Account(name, balance) {
+  this.name = name;
+  this.balance = balance;
+}
+
+Account.prototype.deposit = function (amount) {
+  if (this._isPositive(amount)) {
+    this.balance += amount;
+    console.info(`Deposit: ${this.name} new balance is ${this.balance}`);
+    return true;
+  }
+  return false;
+};
+
+Account.prototype.withdraw = function (amount) {
+  if (this._isAllowed(amount)) {
+    this.balance -= amount;
+    console.info(`Withdraw: ${this.name} new balance is ${this.balance}`);
+    return true;
+  }
+  return false;
+};
+
+Account.prototype.transfer = function (amount, account) {
+  if (this.withdraw(amount) && account.deposit(amount)) {
+    console.info(
+      `Transfer: ${amount} has been moved from ${this.name} to ${account.name}`
+    );
+    return true;
+  }
+  return false;
+};
+
+Account.prototype._isPositive = function (amount) {
+  const isPositive = amount > 0;
+  if (!isPositive) {
+    console.error("Amount must be positive!");
+    return false;
+  }
+  return true;
+};
+
+Account.prototype._isAllowed = function (amount) {
+  if (!this._isPositive(amount)) return false;
+
+  const isAllowed = this.balance - amount >= 0;
+  if (!isAllowed) {
+    console.error("You have insufficent funds!");
+    return false;
+  }
+  return true;
+};
+
+// const amount = document.getElementById("userDeposit").value;
+// const select = document.getElementById("ddlDeposit");
+// const option = select.options[select.selectedIndex];
+// const name = option.text;
+
+window.addEventListener("DOMContentLoaded", () => {
+  console.log("name:", name, ", amount:", amount);
+  console.log("Option:", name);
+
+  const newAcc = new Account(name, amount);
+  acctBalanceLbl.innerText += `${newAcc.name}: ${newAcc.balance}\n`;
+});
+// const userName = new Account(name, amount);
+// const Juma = new Account("Juma", 0);
+// const Linda = new Account("Linda", 111);
+
+//   acctBalanceLbl.innerText += `before:  Wanjiru: ${Wanjiru.balance}, Juma: ${Juma.balance}\n`;
+const depositBtn = document.getElementById("btnDeposit");
+depositBtn.addEventListener("click", () => {
+  const amount = document.getElementById("userDeposit").value;
+  const select = document.getElementById("ddlDeposit");
+  const option = select.options[select.selectedIndex];
+  const name = option.text;
+  const newAcc = new Account(name, amount);
+  Object.keys(newAcc).forEach(key => {
+    //  console.log('array', newAcc[key])
+    window.localStorage.setItem("Accounts", JSON.stringify(newAcc[key]));
   });
-
-// accept deposits from user, store deposits in array
-btnDeposit.addEventListener('click', () => {
-
-    // checks if deposit is a number
-    if (isNaN(userDeposit.value)) {
-        alert("Please enter a number.");
-        return userDeposit.value = '';
-    } else {
-
-    // checks if deposit meets parameters
-        if (userDeposit.value < 0.01 || userDeposit.value > 10000) {
-            alert("You can only deposit between $0.01 and $10,000.")
-            return userDeposit.value = '';
-        } else {
-    
-        // push deposit to array
-        deposits.push(Number(userDeposit.value));
-        // calculate Total Balance
-        totalBalance += (Number(userDeposit.value));
-
-        // format TotalBalance to show $ XX.XX (2 decimal places)
-        let totalBalanceFormatted = formatter.format(totalBalance);
-        document.getElementById("acctBalanceLbl").innerHTML = totalBalanceFormatted;
-
-    // print deposit to console to verify success
-    console.log("$" + userDeposit.value);
-    return userDeposit.value = '';
-    }
-}
-    
 });
+// change to ${customerA}.transfer(${amount}, ${customerB})
+// Wanjiru.transfer(30, Linda);
 
-// accept withdrawals from user, store withdrawals in array
-btnWithdraw.addEventListener('click', () => {
+// acctBalanceLbl.innerText += `Wanjiru: ${Wanjiru.balance}, Juma: ${Juma.balance}, Linda: ${Linda.balance}\n`;
+// const userWithdraw = document.getElementById("userWithdraw");
+// btnWithdraw.addEventListener("click", () => {
 
-    // checks if withdrawal is a number
-    if (isNaN(userWithdraw.value)) {
-        alert("Please enter a number.");
-        return userWithdraw.value = '';
-    } else {
+// });
 
-        // checks if withdrawal meets parameters
-        if (userWithdraw.value > totalBalance - 5) {
-            alert("Your total balance cannot drop below $5.");
-            return userWithdraw.value = '';
-        } else {
+// btnTransfer.addEventListener("click", () => {
 
-        // push withdrawal to array
-        withdrawals.push(Number(userWithdraw.value));
-        // calculate Total Balance
-        totalBalance -= (Number(userWithdraw.value));
-
-        // format TotalBalance to show $ XX.XX (2 decimal places)
-        let totalBalanceFormatted = formatter.format(totalBalance);
-        document.getElementById("acctBalanceLbl").innerHTML = totalBalanceFormatted;
-
-    // print withdrawal to console to verfify success
-    console.log("$" + userWithdraw.value);
-    return userWithdraw.value = '';
-    }
-}
-});
-
-// format TotalBalance to show $ XX.XX (2 decimal places)
-
-let totalBalanceFormatted = formatter.format(totalBalance);
-document.getElementById("acctBalanceLbl").innerHTML = totalBalanceFormatted;
+// });
